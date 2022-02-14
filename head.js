91acc2af9f9c0f47c8db4320afed85c09e9a6194
warlocksa <warlock_sa@hotmail.com>
2022-02-14 15:30:17 -0700

    create test function

--- head.js
+++ head.js
@@ -15,5 +15,5 @@ const assertEqual = require('./assertEqual');
 const head = function(arr) {
   return arr[0];
 }
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+module.exports = head;
