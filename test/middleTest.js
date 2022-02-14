75b7a1edf81647c9a2e0f69fcdde718bdc60e7b1
warlocksa <warlock_sa@hotmail.com>
2022-02-14 15:50:53 -0700

    test files created

---
+++ test/middleTest.js
@@ -0,0 +1,8 @@
+const eqArrays = require('../eqArrays')
+const assertArrayequal = require('../assertArraysEqual')
+const middle = require('../middle') 
+
+console.log(middle([1, 2]));
+console.log(middle([1, 2, 3, 4, 5, 6, 7]));
+console.log(middle([1, 2, 3, 4, 5, 6]));
+// ...
